a2c10f43fe3a58bc23bb9a25e4e938fe16a271ea
Jan Tacakiewicz <Epim3dium@protonmail.com>
2025-10-05 19:54:31 +0200

    audio: added SoundSource that can play audio relative to observer

---
+++ src/audio/sound_source.cpp
@@ -0,0 +1,40 @@
+#include "sound_source.hpp"
+#include <al.h>
+namespace emp {
+SoundSource::SoundSource(const SoundAsset &sound_asset)
+{
+    alGenBuffers(1, &m_buffer);
+
+    alBufferData(m_buffer, sound_asset.format(), sound_asset.pcmBuffer().data(),
+                 static_cast<ALsizei>(sound_asset.pcmBuffer().size() * sizeof(int16_t)),
+                 static_cast<ALsizei>(sound_asset.sample_rate()));
+
+    alGenSources(1, &m_source);
+    alSourcei(m_source, AL_BUFFER, m_buffer);
+}
+SoundSource::~SoundSource()
+{
+    alDeleteSources(1, &m_source);
+    alDeleteBuffers(1, &m_buffer);
+}
+void SoundSource::setRolloff(float rolloff)
+{
+    alSourcef(m_source, AL_ROLLOFF_FACTOR, rolloff);
+}
+void SoundSource::setReferenceDistance(float ref_distance)
+{
+    alSourcef(m_source, AL_REFERENCE_DISTANCE, ref_distance);
+}
+void SoundSource::setMaxDistance(float max_distance)
+{
+    alSourcef(m_source, AL_MAX_DISTANCE, max_distance);
+}
+void SoundSource::play()
+{
+    alSourcePlay(m_source);
+}
+void SoundSource::setPosition(vec3f pos)
+{
+    alSource3f(id(), AL_POSITION, pos.x, pos.y, pos.z);
+}
+}
---
+++ src/audio/sound_source.hpp
@@ -0,0 +1,39 @@
+#ifndef EMP_SOUND_SOURCE_HPP
+#define EMP_SOUND_SOURCE_HPP
+#include "audio/sound_asset.hpp"
+#include "math/math_defs.hpp"
+#include <al.h>
+namespace emp {
+struct SoundSystem;
+
+class SoundSource {
+private:
+    ALuint m_source = -1;
+    ALuint m_buffer;
+
+    void setPosition(vec3f pos);
+
+public:
+    SoundSource() { }
+    SoundSource(const SoundAsset &sound_asset);
+    ~SoundSource();
+    void setRolloff(float rolloff);
+    void setReferenceDistance(float ref_distance);
+    void setMaxDistance(float max_distance);
+    void play();
+    ALuint id() const { return m_source; }
+    ALint state() const
+    {
+        ALint state;
+        alGetSourcei(id(), AL_SOURCE_STATE, &state);
+        return state;
+    }
+    bool isPlaying() const { return state() == AL_PLAYING; }
+    bool isPaused() const { return state() == AL_PAUSED; }
+    bool isStopped() const { return state() == AL_STOPPED; }
+
+    friend SoundSystem;
+};
+
+};
+#endif
