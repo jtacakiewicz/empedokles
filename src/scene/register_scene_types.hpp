6969f303571aff1e64a198ab337cd8d6215db8be
Jan Tacakiewicz <Epim3dium@protonmail.com>
2025-10-05 21:08:17 +0200

    register_scene_types: added SoundSource to registered types

--- src/scene/register_scene_types.hpp
+++ src/scene/register_scene_types.hpp
@@ -1,23 +1,21 @@
 #ifndef EMP_REGISTER_SCENE_TYPES_HPP
 #define EMP_REGISTER_SCENE_TYPES_HPP
+#include "audio/sound_source.hpp"
 #include "core/coordinator.hpp"
 #include "graphics/animated_sprite.hpp"
-#include "graphics/animated_sprite_system.hpp"
 #include "graphics/model.hpp"
 #include "graphics/particle_system.hpp"
-#include "graphics/sprite_system.hpp"
-#include "graphics/texture.hpp"
 #include "physics/collider.hpp"
 #include "physics/constraint.hpp"
 #include "physics/material.hpp"
 #include "physics/physics_system.hpp"
-#include "scene/behaviour.hpp"
 #include "scene/transform.hpp"
 #include "templates/type_pack.hpp"
 namespace emp {
 struct Device;
 
-typedef TypePack<Transform, Constraint, Material, Collider, Rigidbody, Model, ParticleEmitter, Sprite, AnimatedSprite>
+typedef TypePack<Transform, Constraint, Material, Collider, Rigidbody, Model, ParticleEmitter, Sprite, AnimatedSprite,
+                 SoundSource>
     AllComponentTypes;
 
 void registerSceneTypes(Coordinator &ECS);
